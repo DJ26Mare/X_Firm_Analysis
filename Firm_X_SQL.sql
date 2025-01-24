5a4156c17540034a471a1c7780703dccd5209fb1
Dewansh Jain <jain.dewansh26@gmail.com>
2025-01-25 01:33:20 +0530

    Q1 Done

--- Firm_X_SQL.sql
+++ Firm_X_SQL.sql
@@ -1,1 +1,15 @@
-Select * From "Firm1";  -- Case sensitive so "" used
+Select * From "Firm1";
+-- Drop Table "Firm1";
+Select count(*) From "Firm1";
+
+
+-- 	 ## Business Problems ##
+
+--Q1 Find different payment method and no of transactions ,no of quantity sold.
+
+Select payment_method , count(*) as no_of_transactions , Sum(quantity) as no_of_quantitySold
+From "Firm1"
+group by payment_method;
+
+
+--Q2 Identify the highest-rated category in each branch, displaying the branch, category ,AVG RATING
